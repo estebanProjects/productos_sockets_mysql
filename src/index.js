407118044c413a0d95958e5b4e519970f972dd2c
Esteban Espinoza <estebanespinoza1905@gmail.com>
2022-01-03 13:26:40 -0500

    Reto completado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const knex = require("./db")
 const path = require('path')
+const session = require('express-session')
+const mongoStore = require("connect-mongo");
+
 const app = express()
 
 const ContenedorProductos = require("./contenedor")
@@ -14,6 +17,19 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+//mongodb
+app.use(
+    session({
+      store: mongoStore.create({
+        mongoUrl:
+          "mongodb+srv://Esteban:esteban343@coderdesafio.zqmpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      }),
+      secret: "misecreto",
+      saveUninitialized: true,
+      resave: true,
+    })
+  );
 
 
 // Server 
@@ -31,12 +47,54 @@ app.set('views', path.join(__dirname, 'views'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
 
+
+// Router - session
+app.get("/login", (req, res) => {
+    res.render('login.html')
+});
+
+app.post("/login", async(req, res) => {
+    const user = req.body;
+    if (user.username != "") {
+      let username = user.username  
+      console.log(user.username);
+      let msjbienvenida = `Bienvenido ${user.username}`
+      req.session.user = user.username;
+        
+      let productos = await contendProd.getAll()
+      let mensajes = await contendMensj.getAll()
+  
+      res.render('index.html', {productos, mensajes, msjbienvenida, username})
+    //   res.redirect('/')
+      return;
+    }
+    res.redirect("/login");
+  });
+
+  app.get('/logout', (req, res) => {
+    const username = req.session.username
+    if (username) {
+        req.session.destroy(err => {
+            if (!err) {
+                res.render('logout.html', { username })
+            } else {
+                res.redirect('/login')
+            }
+        })
+    } else {
+        res.redirect('/login')
+    }
+  })
+
 // Routes
 app.get('/', async (req, res) => {
     let productos = await contendProd.getAll()
     let mensajes = await contendMensj.getAll()
 
-    res.render('index.html', {productos, mensajes})
+    let msjbienvenida = `Bienvenido`
+    let username = ''
+
+    res.render('index.html', {productos, mensajes, msjbienvenida, username})
 })
 
 app.post('/', async(req, res) => {
